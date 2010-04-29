3c3b5ded093daf3b8a1581a420656b503373acb0
Dmitry Goncharov <dgoncharov@users.sf.net>
2010-04-29 20:16:08 +0400

    initial commit

---
+++ g722dec.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <unistd.h> 
+#include <fcntl.h>
+
+#include "g722.h"
+
+int main(int argc, char const* argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s g722file\n", argv[0]);
+        return 1;
+    }
+    int const infd = open(argv[1], O_RDONLY);
+    if (infd < 0)
+    {
+        fprintf(stderr, "cannot open %s, open(): %s\n", argv[1], strerror(errno));
+        return 2;
+    }
+    struct stat st;
+    if (fstat(infd, &st) < 0)
+    {
+        fprintf(stderr, "cannot get file size, fstat(): %s\n", strerror(errno));
+        return 3;
+    }
+    char buf[st.st_size];
+    int const n = read(infd, buf, st.st_size);
+    if (n < st.st_size)
+    {
+        fprintf(stderr, "read(): %s\n", strerror(errno));
+        return 4;
+    }
+    int16_t outbuf[st.st_size * 10];
+
+    g722_decode_state_t state;
+    g722_decode_init(&state, 64000, 0);
+    int const outlen = g722_decode(&state, outbuf, (const uint8_t*)buf, st.st_size);
+
+    if (write(STDOUT_FILENO, (char*)outbuf, outlen * sizeof(int16_t)) < outlen * sizeof(int16_t))
+    {
+        fprintf(stderr, "write(): %s\n", strerror(errno));
+        return 5;
+    }
+    return 0;
+}
+

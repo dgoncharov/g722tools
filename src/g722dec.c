362f7cc2d7d87c4e2181953e6ad01fbd175f06bb
Dmitry Goncharov <dgoncharov@users.sf.net>
2010-05-12 12:29:01 +0400

    Reading from stdin writing to stdout. Command line options evh

--- src/g722dec.c
+++ src/g722dec.c
@@ -19,42 +19,69 @@
 
 #include "g722.h"
 
-int main(int argc, char const* argv[])
+/* silence warnings in -ansi -pedantic mode*/
+int getopt(int argc, char * const argv[], const char *optstring);
+extern int optind;
+
+void print_help()
 {
-    if (argc != 2)
+    fprintf(stderr, "g722dec [-evh]\n");
+}
+
+int main(int argc, char* argv[])
+{
+    int flags = 0;
+    int opt;
+    while ((opt = getopt(argc, argv, "evh")) != -1)
     {
-        fprintf(stderr, "Usage: %s g722file\n", argv[0]);
-        return 1;
+        switch (opt)
+        {
+        case 'e':
+            flags |= G722_SAMPLE_RATE_8000;
+            break;
+        case 'v':
+        {
+            char const version[] = "0.01";
+            printf("%s\n", version);
+            return 0;
+        }
+        case 'h':
+            print_help();
+            return 0;
+        default:
+            print_help();
+            return 2;
+        }
     }
-    int const infd = open(argv[1], O_RDONLY);
-    if (infd < 0)
+    if (optind > argc)
     {
-        fprintf(stderr, "cannot open %s, open(): %s\n", argv[1], strerror(errno));
-        return 2;
-    }
-    struct stat st;
-    if (fstat(infd, &st) < 0)
-    {
-        fprintf(stderr, "cannot get file size, fstat(): %s\n", strerror(errno));
+        print_help();
         return 3;
     }
+
+    struct stat st;
+    if (fstat(STDIN_FILENO, &st) < 0)
+    {
+        perror("cannot get input file size");
+        return 4;
+    }
     char buf[st.st_size];
-    int const n = read(infd, buf, st.st_size);
+    int const n = read(STDIN_FILENO, buf, st.st_size);
     if (n < st.st_size)
     {
-        fprintf(stderr, "read(): %s\n", strerror(errno));
-        return 4;
+        perror("cannot read");
+        return 5;
     }
     int16_t outbuf[st.st_size * 10];
 
     g722_decode_state_t state;
-    g722_decode_init(&state, 64000, 0);
+    g722_decode_init(&state, 64000, flags);
     int const outlen = g722_decode(&state, outbuf, (const uint8_t*)buf, st.st_size);
 
     if (write(STDOUT_FILENO, (char*)outbuf, outlen * sizeof(int16_t)) < outlen * sizeof(int16_t))
     {
-        fprintf(stderr, "write(): %s\n", strerror(errno));
-        return 5;
+        perror("cannot write");
+        return 6;
     }
     return 0;
 }
